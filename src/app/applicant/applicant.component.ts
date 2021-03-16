c5faf117f5b2878071b526ddb3dc3209c19080e8
EvanHanson234 <EvanHanson234@users.noreply.github.com>
2021-03-16 16:14:57 -0400

    More on forms

--- src/app/applicant/applicant.component.ts
+++ src/app/applicant/applicant.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
-
-import { FormControl } from '@angular/forms';
+//import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './applicant.component.html',
-
   selector: 'app-name-editor',
   //styleUrls: ...
 })
+
 export class ApplicantComponent {
-  profileName = new FormControl('');
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+    street: [''],
+    city: [''],
+    state: [''],
+    zip: ['']
+    })
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  updateName() {
+    //this.profileFirstName.setValue('Nancy');
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address:{
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  onSubmit(){
+    console.warn(this.profileForm.value);
+  }
 }
