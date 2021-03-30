cfbbd236cc2e03e88258b2c6258d561daa2ec9aa
EvanHanson234 <EvanHanson234@users.noreply.github.com>
2021-03-30 16:23:28 -0400

    Created new router for company user page

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Company, CompanyService } from './companies.service';
 
+import { Router } from '@angular/router';
+
 @Component({
   templateUrl: './companies.component.html'
 })
@@ -9,12 +11,16 @@ export class CompaniesComponent implements OnInit {
   ready = false;
   companies: Company[];
 
-  constructor(private companyService: CompanyService) { }
+  constructor(private companyService: CompanyService, private router: Router) { }
 
   ngOnInit(): void {
     this.companyService.getAll().subscribe(companies => {
       this.companies = companies;
       this.ready = true;
     });
+  }
+
+  updateProfile(){
+    this.router.navigate(['/js_profile'])
   }
 }
