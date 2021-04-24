fd91f4f75e0aa93157911a6fdde5407e6514f18d
WSteuk4 <64223501+WSteuk4@users.noreply.github.com>
2021-04-24 14:31:19 -0400

    view data for jobseekers

--- src/app/js_profile/js-profile.service.ts
+++ src/app/js_profile/js-profile.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+export interface Client {
+  first_name: string;
+  last_name: string;
+  date_of_birth: Date;
+  sex: string;
+  phone_number: string;
+  email_address: string;
+  address: string;
+  owns_car: boolean;
+  has_license: boolean;
+  ride_available: boolean;
+  status_id: number;
+  experience: string;
+  job_interests: string;
+  skills: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsProfileService {
+  private server = environment.server;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getByID(userId: string): Observable<Client> {
+    return this.http.get<Client>(`${this.server}/api/jobseeker_users/${userId}`);
+  }
 }
