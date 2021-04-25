6c3d5969db6f37f21ba2ac0ac0d2e8e0bca8e1db
WSteuk4 <64223501+WSteuk4@users.noreply.github.com>
2021-04-25 12:46:13 -0400

    Variable Name Changes

--- src/app/js_profile/js-profile.service.ts
+++ src/app/js_profile/js-profile.service.ts
@@ -5,19 +5,29 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 export interface Client {
+  id: string;
   first_name: string;
   last_name: string;
   date_of_birth: Date;
   sex: string;
+  support_contact: string;
   phone_number: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
   email_address: string;
-  address: string;
+  job_1: string;
+  empl_1: string;
+  job_2: string;
+  empl_2: string;
+  job_3: string;
+  empl_3: string;
   owns_car: boolean;
   has_license: boolean;
   ride_available: boolean;
-  status_id: number;
-  experience: string;
   job_interests: string;
+  status_id: number;
   skills: string;
 }
 
