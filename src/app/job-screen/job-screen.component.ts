96132e3181274a800f35f5c1d71586ddd1e64857
Patrick <pmanrecc@gmail.com>
2021-04-27 17:13:32 -0400

    job screen addition

---
+++ src/app/job-screen/job-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-screen',
+  templateUrl: './job-screen.component.html',
+  styles: [
+  ]
+})
+export class JobScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
